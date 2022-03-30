5978856f71e6230d1e75d41c33ec59444f4bbb55
unknown <1nivla1alvin@gmail.com>
2022-03-30 20:37:17 +0700

    update migration

--- database/migrations/2022_03_30_110228_create_jenis_table.php
+++ database/migrations/2022_03_30_110228_create_jenis_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRoomTable extends Migration
+class CreateJenisTable extends Migration
 {
     /**
      * Run the migrations.
